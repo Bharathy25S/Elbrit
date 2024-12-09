a3fd71dee58ae4e7591342224c153fa7e39e5016
Bharathy25S <146206293+Bharathy25S@users.noreply.github.com>
2024-12-09 23:56:57 +0530

    Delete eden-pharmacy-landing-page directory

--- eden-pharmacy-landing-page/components/Header.js
+++
@@ -1,9 +0,0 @@
-import styles from './Header.module.css';
-
-const Header = () => (
-  <header className={styles.header}>
-    <img src="/logo.jpeg" alt="Eden Pharmacy Logo" className={styles.logo} width='150px'/>
-  </header>
-);
-
-export default Header;
--- eden-pharmacy-landing-page/components/LazyImage.js
+++
@@ -1,7 +0,0 @@
-import Image from 'next/image';
-
-const LazyImage = ({ src, alt }) => (
-  <Image src={src} alt={alt} width={600} height={400} loading="lazy" />
-);
-
-export default LazyImage;
